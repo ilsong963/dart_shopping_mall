5db3d912585af8dcc9dfb7ad4b9212cd2842d48e
yooilsong <ilsong963@naver.com>
2025-03-14 03:16:07 +0900

    init

---
+++ bin/dart_shopping_mall.dart
@@ -0,0 +1,5 @@
+import 'package:dart_shopping_mall/dart_shopping_mall.dart' as dart_shopping_mall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_shopping_mall.calculate()}!');
+}
---
+++ test/dart_shopping_mall_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_shopping_mall/dart_shopping_mall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
